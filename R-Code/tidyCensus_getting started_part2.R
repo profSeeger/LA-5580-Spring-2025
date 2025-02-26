564a8f72afe5125fec60c83785bcdcd35f785aa6
Christopher J. Seeger <cjseeger@iastate.edu>
2025-02-26 15:21:51 -0600

    poverty

--- R-Code/tidyCensus_getting started_part2.R
+++ R-Code/tidyCensus_getting started_part2.R
@@ -35,7 +35,11 @@ library(highcharter)
 
 
 
-#Display all the variable in the 2023 ACS and open a tab to view the info
+#Display all the variable in the 2023 1 year ACS and open a tab to view the info
+ACS1_2023_variables <- load_variables(2023, "acs1", cache = TRUE)
+View(ACS1_2023_variables)
+
+#for ACS5
 ACS5_2023_variables <- load_variables(2023, "acs5", cache = TRUE)
 View(ACS5_2023_variables)
 
@@ -47,37 +51,74 @@ View(ACS5_2023_variables)
 # B05010_002 total number with a ratio value below 1, by definition in poverty
 
 # Now that we have two variables to work with, we want to retrieve them from 
-# The US Census using a package called tidyCensus
+# The US Census using a package called TidyCensus
 
 # Save the results of this in data frame which I am calling poverty_df
 
-poverty_df <- get_acs(geography = "county", 
-              variables = "B05010_002",
+#Example looking at ACS1 data, remove the survey = "acs1" to use acs5
+poverty_est_df <- get_acs(geography = "county", 
+              variables = "B05010_001",
               state = "Iowa", 
               output = "wide",
               geometry = TRUE, 
+              survey = "acs1",
               year = 2023)
+#look at the data frame and notice only 10 results!
+view(poverty_est_df)
+
+#maybe preview this as a map
+#add Mapview package and library. In the future include this at the top of script!
+install.packages("mapview")
+library(mapview)
+
+mapview(poverty_est_df)
 
 
 
+## ----Getting down to business---------------------------------------------
+
+# Before we get to far, we need to set our working directory. So first save this file.
+# From the Session Menu --> Set Working Directory --> To Source File Location
+getwd() #this will display your working directory
+
+#Get the ACS5 data, remove the survey = "acs1" to use acs5
+poverty_est_df <- get_acs(geography = "county", 
+                          variables = "B05010_001",
+                          state = "Iowa", 
+                          output = "wide",
+                          geometry = TRUE,
+                          year = 2023)
+mapview(poverty_est_df)
+
+#now get the B05010_002 total number with a ratio value below 1
+poverty_under1_df <- get_acs(geography = "county", 
+                          variables = "B05010_002",
+                          state = "Iowa", 
+                          output = "wide",
+                          geometry = TRUE,
+                          year = 2023)
+mapview(poverty_under1_df)
 
 
+#but now we have two data frames, it would be better if we had this in just one
+poverty_df <- get_acs(geography = "county", 
+                      variables = c(poverty_est = "B05010_001",
+                                    poverty_und1 = "B05010_002"),
+                      state = "Iowa", 
+                      output = "wide",
+                      geometry = TRUE,
+                      year = 2023)
+view(poverty_df)
+mapview(poverty_df)
 
+#Looks good so we can create a shapefile
+st_write(poverty_df, "iowa_poverty.shp")
 
+#note error message about ESRI field name abbreviation
+#so save as a geojson!
+st_write(poverty_df, "iowa_poverty.geojson")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#If we just wanted the CV, we could run the code above with geometry = FALSE
 
 
 
